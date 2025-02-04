1b4e4c885acc9712c32fea649b52d1e7c73b8ec0
jaozinf <202407358195@alunos.estacio.br>
2025-02-04 22:13:41 +0000

    ajuste nas cidades

--- desafio trunfo novato.c
+++ desafio trunfo novato.c
@@ -2,20 +2,28 @@
 
 int main(){
 
-    printf("Desafio Super Trunfo!\n");
+    printf("Desafio Super Trunfo paises!\n");
     
-    char nome[100];
-    int codigo;
+    char pais[16];
+    char estado[15];
+    char cidade[14];
+    char codigo[4];
     int populacao;
     float area;
     float pib;
     int pontos;
 
     printf("Digite o nome do pais: \n");
-    scanf("%s", &nome);
+    scanf("%s", &pais);
     
-    printf("Digite o código do pais: \n");
-    scanf("%d", &codigo);
+    printf("Digite o nome do estado: \n");
+    scanf("%s", &estado);
+
+    printf("Digite o nome da cidade: \n");
+    scanf("%s", cidade);
+
+    printf("Digite o código da cidade: \n");
+    scanf("%s", &codigo);
 
     printf("Digite a população : \n");
     scanf("%d", &populacao);
@@ -29,14 +37,18 @@ int main(){
     printf("Digite o número dos pontos turisticos: \n ");
     scanf("%d", &pontos);
 
-    printf("nome: %s\n", nome);
-    printf("codigo: %d\n", codigo);
+    printf("nome: %s\n", pais);
+    printf("estado: %s\n", estado);
+    printf("cidade: %s\n", cidade);
+    printf("codigo: %s\n", codigo);
     printf("populacao: %d\n", populacao);
     printf("area: %f\n", area);
     printf("pib: %F\n", pib);
     printf("pontos turisticos: %d\n", pontos);
     printf("Agora vamos cadastrar outras cartas! \n");
     
+    printf("novo commit\n");
+
     return 0;
 
 }
